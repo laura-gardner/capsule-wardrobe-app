101284df520e1abf2653958b5870ad65812c6af9
laura-gardner <elgardner@gmail.com>
2022-11-17 11:48:26 +0000

    move HTML for displaying capsule items so generated by JS to allow for implementation of form to get url for web based images

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const topsDiv = document.querySelector('.tops');
-
 const tops = ['img/top1.png', 'img/top2.png', 'img/top3.png', 'img/top4.png']
 const bottoms = ['img/bottom1.png', 'img/bottom2.png', 'img/bottom3.png', 'img/bottom4.png']
 const bottomsDiv = document.querySelector('.bottoms');
 const capsuleOutfits = document.querySelector('.capsule-outfits');
+
+function displayCapsuleItems (topsArray, bottomsArray) {
+    const capsuleItems = document.querySelector('.capsule-items');
+    capsuleItems.innerHTML = `
+        <h2>Capsule items: tops</h2>
+        <div class="tops">
+            <div class="top">
+                <img src="${topsArray[0]}" alt="">
+            </div>
+            <div class="top">
+                <img src="${topsArray[1]}" alt="">
+            </div>
+            <div class="top">
+                <img src="${topsArray[2]}" alt="">
+            </div>
+            <div class="top">
+                <img src="${topsArray[3]}" alt="">
+            </div>
+        </div>
+        <h2>Capsule items: bottoms</h2>
+        <div class="bottoms">
+            <div class="bottom">
+                <img src="${bottomsArray[0]}" alt="">
+            </div>
+            <div class="bottom">
+                <img src="${bottomsArray[1]}" alt="">
+            </div>
+            <div class="bottom">
+                <img src="${bottomsArray[2]}" alt="">
+            </div>
+            <div class="bottom">
+                <img src="${bottomsArray[3]}" alt="">
+            </div>
+        </div>
+    `
+}
+
+displayCapsuleItems(tops, bottoms);
 
 function makeOutfit (bottomsArrayIndex) {
     tops.forEach(top => {
