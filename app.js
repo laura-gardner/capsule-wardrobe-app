45642be52de109fa519226f0f00744293eaf23f2
laura-gardner <elgardner@gmail.com>
2022-11-18 10:24:26 +0000

    Add functionality to take in urls for images.  Add random outfit generator

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const topsDiv = document.querySelector('.tops');
-const tops = ['img/top1.png', 'img/top2.png', 'img/top3.png', 'img/top4.png']
-const bottoms = ['img/bottom1.png', 'img/bottom2.png', 'img/bottom3.png', 'img/bottom4.png']
 const bottomsDiv = document.querySelector('.bottoms');
 const capsuleOutfits = document.querySelector('.capsule-outfits');
+
+const capsuleForm = document.querySelector('.capsule-form');
+const randomOutfitButton = document.querySelector('.random-outfit');
+const allOutfitsButton = document.querySelector('.all-outfits');
+
+capsuleForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const top1 = e.target[0].value;
+    const top2 = e.target[1].value;
+    const top3 = e.target[2].value;
+    const top4 = e.target[3].value;
+    const bottom1 = e.target[4].value;
+    const bottom2 = e.target[5].value;
+    const bottom3 = e.target[6].value;
+    const bottom4 = e.target[7].value;
+    const tops = [top1, top2, top3, top4]
+    const bottoms = [bottom1, bottom2, bottom3, bottom4]
+    if (e.submitter.className === "all-outfits") {    
+        displayCapsuleItems(tops, bottoms);
+        for (let i=0; i<=bottoms.length-1; i++) {
+            makeOutfit(tops, bottoms, i)
+        };
+    } else if (e.submitter.className === 'random-outfit') {
+        randomOutfit(tops, bottoms);
+    }
+});
 
 function displayCapsuleItems (topsArray, bottomsArray) {
     const capsuleItems = document.querySelector('.capsule-items');
@@ -40,10 +64,10 @@ function displayCapsuleItems (topsArray, bottomsArray) {
     `
 }
 
-displayCapsuleItems(tops, bottoms);
 
-function makeOutfit (bottomsArrayIndex) {
-    tops.forEach(top => {
+
+function makeOutfit (topsArray, bottomsArray, bottomsArrayIndex) {
+    topsArray.forEach(top => {
         const newOutfitDiv = document.createElement('div');
         const topDiv = document.createElement('div');
         const bottomDiv = document.createElement('div');
@@ -53,7 +77,7 @@ function makeOutfit (bottomsArrayIndex) {
         const imgTop = document.createElement('img');
         imgTop.src = top;
         const imgBottom = document.createElement('img');
-        imgBottom.src = bottoms[bottomsArrayIndex];
+        imgBottom.src = bottomsArray[bottomsArrayIndex];
         topDiv.appendChild(imgTop);
         bottomDiv.appendChild(imgBottom);
         newOutfitDiv.appendChild(topDiv);
@@ -62,6 +86,24 @@ function makeOutfit (bottomsArrayIndex) {
     })
 }
 
-for (let i=0; i<=bottoms.length-1; i++) {
-    makeOutfit(i)
-};
+function randomOutfit (topsArray, bottomsArray) {
+    const randTopsArrayIndex = Math.floor(Math.random() * 3);
+    const randBottomsArrayIndex = Math.floor(Math.random() * 3);
+    const newOutfitDiv = document.createElement('div');
+    const topDiv = document.createElement('div');
+    const bottomDiv = document.createElement('div');
+    capsuleOutfits.innerHTML = ""
+    topDiv.classList.add('top');
+    bottomDiv.classList.add('bottom');
+    newOutfitDiv.classList.add('outfit');
+    const imgTop = document.createElement('img');
+    imgTop.src = topsArray[randTopsArrayIndex];
+    const imgBottom = document.createElement('img');
+    imgBottom.src = bottomsArray[randBottomsArrayIndex];
+    topDiv.appendChild(imgTop);
+    bottomDiv.appendChild(imgBottom);
+    newOutfitDiv.appendChild(topDiv);
+    newOutfitDiv.appendChild(bottomDiv);
+    capsuleOutfits.appendChild(newOutfitDiv); 
+
+}
